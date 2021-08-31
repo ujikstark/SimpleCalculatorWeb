468a296496f4337d7cdf232452a45fee8390e0ca
ujikstark <57621743+ujikstark@users.noreply.github.com>
2021-08-31 19:17:56 +0700

    Update app.js

--- app.js
+++ app.js
@@ -11,12 +11,13 @@ for (let i = 0; i < buttonList.length; i++) {
 
     buttonList[i].addEventListener('click', () => {
         
+        // check if the current element is not a number
         if (isNaN(buttonList[i].value) && 
-        buttonList[i].value != 'clear' &&
-        buttonList[i].value != 'remove') {
-            op = buttonList[i].value;
-            num = 0;
-        } else if (!isNaN(buttonList[i].value)) {
+            buttonList[i].value != 'clear' &&
+            buttonList[i].value != 'remove') {
+                op = buttonList[i].value;
+                num = 0;
+        } else if (!isNaN(buttonList[i].value)) { // the current element is a number
             if (show.innerHTML.length > 0) {
                 if (!isNaN(show.innerHTML.charAt(show.innerHTML.length-1))) {
                     num += buttonList[i].value;
@@ -30,12 +31,12 @@ for (let i = 0; i < buttonList.length; i++) {
             
         }
 
-        num = +num;
-        answer = +answer;
+        num = +num; // convert string to integer
+        answer = +answer; // convert string to integer
         
-        console.log(num)
+        // console.log(num)
 
-        show.innerHTML += buttonList[i].value;
+        show.innerHTML += buttonList[i].value; 
 
         if (buttonList[i].value == 'clear') {
             show.innerHTML = '';
@@ -45,28 +46,32 @@ for (let i = 0; i < buttonList.length; i++) {
             answer = 0;
             stack = [];
         }
-
+        
+        // check if the previous element is operator 
+        // and the current element is operator too
         if (isNaN(show.innerHTML.charAt(show.innerHTML.length-2)) &&
             isNaN(buttonList[i].value)) {
+                
+                // change the previous element with the current element
+                // and delete the current element
                 show.innerHTML = show.innerHTML.substring(0, show.innerHTML.length-2) + buttonList[i].value;
         }
         
+        
         if (buttonList[i].className != 'submit' && !isNaN(buttonList[i].value)) {
-            
            
-
             if (op == '+') {
                 
                 answer += num;
                 
-                
+                // save the previous count without adding this current element
                 if (show.innerHTML.length > 0)
                     stack.push(answer - num);
             
             } else if (op == '-') {
                 answer -= num;
                 
-                if (show.innerHTML.length > 1)
+                if (show.innerHTML.length > 0)
                     stack.push(answer + num);
                 
             } else if (op == 'x') {
@@ -74,10 +79,13 @@ for (let i = 0; i < buttonList.length; i++) {
                 if (stack.length == 0) {
                     answer *= num;
                 } else {
+                    // get the saved element    
                     var temp = answer - stack[stack.length-1];
                     
+                    // count saved element with current element
                     temp *= num;
                     
+                    // sum the previous count with the current count
                     answer = stack[stack.length-1] + temp;
                 }
                 
